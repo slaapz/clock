f3339bc71e3dbc43d6d9cfeceda94b657f058e10
Patrick Ball <slaapz@gmail.com>
2023-03-22 20:59:19 -0600

    Created funciton to move he hands

--- clock.js
+++ clock.js
@@ -2,4 +2,23 @@ const hourEl = document.querySelector('.hour');
 const minuteEl = document.querySelector('.minute');
 const secondEl = document.querySelector('.second');
 
+function updateClock() {
+    const current_date = new Date();
+    // setTimeout(() => {
+        // updateClock();
+    // }, 1000);
+    const hour = current_date.getHours();
+    const minute = current_date.getMinutes();
+    const second = current_date.getSeconds();
 
+    const hourDegree = (hour / 12) * 360;
+    hourEl.style.transform = `rotate(${hourDegree}deg)`;
+    const minuteDegree = (minute / 60) * 360;
+    minuteEl.style.transform = `rotate(${minuteDegree}deg)`;
+    const secondDegree = (second / 60) * 360;
+    secondEl.style.transform = `rotate(${secondDegree}deg)`;
+}
+
+// updateClock();
+
+setInterval(updateClock, 1000);
